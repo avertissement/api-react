3e9951d2ff16ea62787790262b0f3dd5f598b3b9
Matthew Day <matt@matthewday.net>
2021-04-21 13:58:07 -0400

    resolve merge conflicts

---
+++ bookmarks/bookmarks/BookmarksList.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './BookmarksList.css';
+import Bookmark from './Bookmark.js';
+
+class BookmarkList extends Component {
+
+  render() {
+    const bookmarks = this.props.bookmarks.map((bookmark, i) => <Bookmark {...bookmark} key={i} />);
+    
+    return (
+      <div className="bookmarkList">
+        {bookmarks}
+      </div>
+    );
+
+  }
+
+}
+
+BookmarkList.defaultProps = {
+  bookmarks: []
+};
+
+export default BookmarkList;
--- demonym/App.js
+++ demonym/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import DemonymApp from './demonymApp.js';
+import BookmarksApp from './bookmarks/BookmarksApp';
+import DemonymApp from './demonym/demonymApp';
 
 export default function App() {
   return (
     <div className="App">
       <DemonymApp />
+      <BookmarksApp />
     </div>
   );
 }
